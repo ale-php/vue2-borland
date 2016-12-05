5c24a550fc627fc723f3a8a84df9cd6da72e46b2
ale-php <alexandre@indev.net.br>
2016-12-05 00:10:04 -0200

    start

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <hello></hello>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+
+export default {
+  name: 'app',
+  components: {
+    Hello
+  }
+}
+</script>
+<!-- caminho para o arquivo sass-->
+<style src="assets/sass/app.scss" lang="sass"></style>
